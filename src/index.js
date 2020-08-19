f078d5c632563b3e4037498e3ba319689e685d38
cgillies93 <59749520+cgillies93@users.noreply.github.com>
2020-08-19 16:35:21 -0700

    add carousel to CampgroundItemPage

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App.js';
 import sampleData from './initialState.json';
-
 import storeFactory from './store';
 import { Provider } from 'react-redux';
 
--- src/pages/CampgroundItemPage.js
+++ src/pages/CampgroundItemPage.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
+import Carousel from 'react-bootstrap/Carousel';
 import CampgroundItemInfo from '../components/ui/CampgroundItemInfo';
 import CampgroundItemDescription from '../components/ui/CampgroundItemDescription';
 import CampgroundAvailability from '../components/ui/CampgroundAvailability';
 import '../components/styles/CampgroundItemPage.css';
+import campingBackground from '../campingBackgroundCrop.jpg';
 
 const CampgroundItemPage = ({ match, campgrounds }) => {
   let id = parseInt(match.params.id)
@@ -11,6 +13,16 @@ const CampgroundItemPage = ({ match, campgrounds }) => {
     return (
         <div className='campground-item-page-container'>
           <section className='campground-item-images'>
+            <Carousel>
+              <Carousel.Item>
+                <img src={campingBackground}
+                     className='carousel-img'/>
+              </Carousel.Item>
+              <Carousel.Item>
+                <img src={campingBackground}
+                     className='carousel-img'/>
+              </Carousel.Item>
+            </Carousel>
           </section>
           <CampgroundItemInfo campground={campground} />
           <CampgroundItemDescription />
